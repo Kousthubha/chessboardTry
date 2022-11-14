0882bbb33cb225dd697ec8dad44b0ffb7f381cc3
Kousthubha <kousthubha.ak@gmail.com>
2022-11-14 18:11:10 +0530

    Create ChessBox.js

---
+++ ChessBox.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function ChessBox(){
+return ( 
+<div className="rect">
+   <div className="blackSquare"></div>
+   <div className="whiteSquare"></div>
+ </div>
+ )
+}
